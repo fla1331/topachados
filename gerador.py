7ef9b6a11fd0d2d20f373ca2634c03fe3eec986d
flanegociosonline-cell <flanegociosonline@gmail.com>
2026-01-10 09:22:24 -0300

    refresh

--- gerador.py
+++ gerador.py
@@ -1648,7 +1648,7 @@ Retorne APENAS o código HTML, sem explicações."""
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{titulo}</title>
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="https://topofertas.reviewnexus.blog/assets/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="https://topofertas.reviewnexus.blog/assets/js/script.js" defer></script>
 </head>
